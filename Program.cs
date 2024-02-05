ea94cc8de3154cb245fe96795fd391fd3ac32d34
Nick Mohale <52918728+nickmohale-collab@users.noreply.github.com>
2024-02-05 08:42:09 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(5);
+            circle.Area();
+            circle.Parimeter();
+
+            Console.WriteLine("Circle Area : " +  circle.Area());
+            Console.WriteLine("Circle Perimeter : " + circle.Parimeter());
+
+            Console.ReadKey();
+        }
+
+        public interface IShape
+        {
+            double Area();
+            double Parimeter();
+        
+        }
+
+        public class  Circle : IShape 
+        {
+            public double Radius { get; set; }
+
+            public Circle(double radius)
+            {
+                Radius = radius;
+            }
+
+            public double Area()
+            {
+                return Math.PI * Radius;
+            }
+
+            public double Parimeter()
+            {
+                return 2 * Math.PI * Radius;
+            }
+        }
+    }
+}
